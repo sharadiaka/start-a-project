53d61b4993462364a0bdc846bc9550bfc87f4e98
breno.oliveira <breno.oliveira@LAB2-DCX-10.labs.dcx>
2020-02-13 08:27:49 -0300

    programa Java

---
+++ testGit/src/testGit/Breno.java
@@ -0,0 +1,16 @@
+package testGit;
+
+
+public class Breno {
+	
+	public static int numeros(int x, int y) {
+		return x + y;
+		
+	}
+	
+	public static void main(String [] args) {
+		int soma = numeros(4,5);
+		System.out.println(soma);
+
+	}
+}
